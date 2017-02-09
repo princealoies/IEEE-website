d0be29f90d1e102fd14f8cb5e844fce8ac77409f
princealoies <princealoies@outlook.com>
2017-02-09 12:50:40 -0330

    Add files via upload

---
+++ js/nivo-slider/_vti_cnf/jquery.nivo.slider.pack.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|10 Sep 2012 23:12:20 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|template_slider.dwt events.html index.html
+vti_author:SR|BrianRoom-PC\\Brian
+vti_modifiedby:SR|BrianRoom-PC\\Brian
+vti_timecreated:TR|10 Sep 2012 23:12:20 -0000
+vti_cacheddtm:TX|10 Sep 2012 23:12:20 -0000
+vti_filesize:IR|12210
